876b4ddb4fc0e48a03199df12c8f43263a6b7a5d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-05 14:24:08 -0500

    [+] Page > log-in: 1. Define form field structure 2. Implement Formik to track error values and visited fields, orchestrate validation, and handle submission 3. Create a data Schema to create validation rules for form fields using Yup

--- pages/log-in.js
+++ pages/log-in.js
@@ -1,11 +1,103 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
+/** Dependencies */
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 /** Component */
 const LogIn = () => {
+
+    /** Form validation */
+    const formik = useFormik({
+        initialValues: {        //  Initial default values of Formik fields
+            email: '',
+            password: ''
+        },
+        /** Create a data Schema to create validation rules using Yup */
+        validationSchema: Yup .object({
+            email: Yup 
+                    .string() 
+                    .email( 'No es un email válido' ) 
+                    .required( 'El email es requerido' ),
+            password: Yup 
+                        .string()
+                        .required( 'La contraseña es requerida' )
+        }),
+        onSubmit: data => {
+            console .log( 'Sending', data );
+        }
+    });
+
     return (
         <Layout>
-            <h1>Log In Page</h1>
+            <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+                
+                <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Iniciar sesión</h2>
+                
+                <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-lg">
+                        <form 
+                            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                            onSubmit={ formik .handleSubmit }
+                        >
+                            
+                            <div className="mb-4">
+                                <label 
+                                    className="block text-black text-sm font-bold mb-2"
+                                    htmlFor="email"
+                                >Email</label>
+                                <input 
+                                    id="email"
+                                    type="email" 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    placeholder="Email del usuario"
+                                    value={ formik .values .email }
+                                    onChange={ formik .handleChange }
+                                    onBlur={ formik .handleBlur }
+                                />
+                                { formik .touched .email && formik .errors .email 
+                                    ?   <div className="my-2 bg-gray-200 border-l-4 border-red-400 text-red-700 p-4">
+                                            <p className="font-bold">Error</p>
+                                            <p>{ formik .errors .email }</p>
+                                        </div> 
+                                    :  null
+                                }
+                            </div>
+                            <div className="mb-4">
+                                <label 
+                                    className="block text-black text-sm font-bold mb-2"
+                                    htmlFor="password"
+                                >Contraseña</label>
+                                <input 
+                                    id="password"
+                                    type="password" 
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    placeholder="Contraseña del usuario"
+                                    value={ formik .values .password }
+                                    onChange={ formik .handleChange }
+                                    onBlur={ formik .handleBlur }
+                                />
+                                { formik .touched .password && formik .errors .password 
+                                    ?   <div className="my-2 bg-gray-200 border-l-4 border-red-400 text-red-700 p-4">
+                                            <p className="font-bold">Error</p>
+                                            <p>{ formik .errors .password }</p>
+                                        </div> 
+                                    :  null
+                                }
+                            </div>
+                            <div className="mb-4">
+                                <button
+                                    type="submit"
+                                    className="bg-red-400 hover:bg-gray-900 rounded w-full p-2 text-white uppercase font-bold"
+                                >Iniciar sesión</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+
+            </div>
         </Layout>
     );
 }
