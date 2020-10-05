a9f42b02bc735d020db267519fc4b4890c1646c6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-05 11:05:26 -0500

    [+] Component > Header: 1. Create login buttons and create account in Header Component 2. Add link to logo

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Link from 'next/link';
 
 /** Component */
 const Header = () => {
     return(
         <header className="py-8 flex flex-col md:flex-row itmes-center justify-between">
-            <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+            <Link href="/">
+                <a>
+                    <img className="w-64 mb-8 md:mb-0" src="logo.svg" />
+                </a>
+            </Link>
+            
+            <div>
+                <Link href="/">
+                    <a className="bg-red-400 px-5 py-3 rounded-lg text-white font-bold uppercase mx-1">Iniciar Sesión</a>
+                </Link>
+                <Link href="/">
+                    <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase mx-1">Crear cuenta</a>
+                </Link>
+            </div>
         </header>
     );
 }
