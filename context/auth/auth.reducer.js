1b300fbb1509f1ce80d45558c2443c80def7d639
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-06 16:06:49 -0500

    [+] Context > auth: 1. Define Reducer 2. Define the State using the Reducer 3. Makes the State properties available to the entire Application

--- context/auth/auth.reducer.js
+++ context/auth/auth.reducer.js
@@ -1,1 +1,7 @@
 /** Reducer are the functions that will modify the State  */
+export default ( state, action ) => {
+    switch( action .type ) {
+        default:
+            return state;
+    }
+}
--- context/auth/auth.state.js
+++ context/auth/auth.state.js
@@ -1,14 +1,29 @@
 import React, { useReducer } from 'react';
 import AuthContext from './auth.context';
+import AuthReducer from './auth.reducer';
 
 /** Define State:
  * Actions that trigger the functions that are in the Reducer
  */
 const AuthState = ({ children }) => {
 
+    const 
+        initialState = {            //  Define State
+            token: '',
+            is_authenticated: null,
+            user: null,
+            msg: null
+        },
+        [ state, dispath ] = useReducer( AuthReducer, initialState );   //  Define Reducer
+
     return(
         <AuthContext .Provider
-            value={{ }}
+            value={{ 
+                token: state .token,
+                is_authenticated: state .is_authenticated,
+                user: state .user,
+                msg: state .msg
+            }}
         >
             { children }
         </AuthContext .Provider>
