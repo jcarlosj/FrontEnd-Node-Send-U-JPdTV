54d5097967fd196aac045dcfe6a794fcb235854c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-06 16:54:53 -0500

    [+] NextJS, Axios: 1. Define environment variable in NextJS 2. Create Axios Client using environment variable with URL to access BackEnd

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** next.config.js is the name of the file where environment variables should be defined in NextJS */
+module .exports = {
+    env: {
+        backendURL: 'http://localhost:4000'
+    }
+}
