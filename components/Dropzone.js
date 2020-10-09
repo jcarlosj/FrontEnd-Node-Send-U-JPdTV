723dacb88d9fda0ad433542fbfeaff9e79fbeefc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-09 14:31:33 -0500

    [+] Component > Dropzone: Implementation of React-Dropzone to the component

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+
+/** Dependencies */
+import { useDropzone } from 'react-dropzone';
+import clientAxios from '../config/axios';
 
 const Dropzone = () => {
+
+    /** Extract properties from Dropzone */
+    const { getRootProps, getInputProps, isDragActive } = useDropzone();
+
     return (
-        <div className="md:p-10 md:flex-1 mb-3 mx-2 mt-6 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-            <p>Dropzone</p>
+        <div className="md:flex-1 mb-3 mx-2 mt-6 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
+            <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
+                <input className="h-100" { ...getInputProps() } />
+    
+                { isDragActive 
+                    ?   <p className="text-2xl text-center text-gray-800">Muy bien, ahora suelta el archivo</p>
+                    :   <div className="text-center">
+                            <p className="text-2xl text-center text-gray-600">Arrastra hasta aquí un archivo</p>
+                            <p className="text-2xl text-center text-gray-600">ó</p>
+                            <button 
+                                className="bg-blue-700 w-full py-2 rounded-lg text-white my-4 hover:bg-blue-800"
+                                type="button"
+                            >Seleccionalo para subir</button>
+                        </div>
+                }
+
+            </div>
         </div>
     );
 }
