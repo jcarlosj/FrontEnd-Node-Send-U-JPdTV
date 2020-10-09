a1fed88d2f39bad864e88652a92c95312edd3cdf
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-09 14:41:28 -0500

    [+] Component > Dropzone: Detects the file to upload

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -6,8 +6,11 @@ import clientAxios from '../config/axios';
 
 const Dropzone = () => {
 
-    /** Extract properties from Dropzone */
-    const { getRootProps, getInputProps, isDragActive } = useDropzone();
+    const onDrop = () => {
+        console .log( 'Soltando Archivo...' );
+    }
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });    //  Extract properties from Dropzone
 
     return (
         <div className="md:flex-1 mb-3 mx-2 mt-6 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
