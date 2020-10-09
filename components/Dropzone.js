580c479a18eb88fad7c6b0ebc2c663a39a5be1be
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-09 14:08:51 -0500

    [+] Component > Dropzone: Create Component and Implement view design

---
+++ components/Dropzone.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dropzone = () => {
+    return (
+        <div className="md:p-10 md:flex-1 mb-3 mx-2 mt-6 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
+            <p>Dropzone</p>
+        </div>
+    );
+}
+
+export default Dropzone;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import AuthContext from '../context/auth/auth.context';
 
 /** Components */
 import Layout from '../components/Layout';
+import Dropzone from '../components/Dropzone';
 
 /** Component */
 const Index = () => {
@@ -25,9 +26,9 @@ const Index = () => {
         <Layout>
             <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
                 <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-                    <div className="md:flex-1 mb-3 mx-2 mt-6 lg:mt-0">
-                        <p>Dropzone</p>
-                    </div>
+                    
+                    <Dropzone />
+                    
                     <div className="md:flex-1 mb-3 mx-2 mt-6 lg:mt-0">
                         <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">Compartir archivos de forma segura y privada</h2>
                         <p className="">
