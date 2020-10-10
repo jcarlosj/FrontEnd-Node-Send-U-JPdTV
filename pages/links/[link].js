56081e78b6f0bd57b91d560ffcde6790d5e25e77
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-10 17:49:26 -0500

    [+] Page > [link]: Configure Dynamic Component for static URL generation using getStaticPaths and getStaticProps

--- pages/links/[link].js
+++ pages/links/[link].js
@@ -1,11 +1,48 @@
 import Layout from '../../components/Layout';
 
+export async function getStaticProps({ params }) {
+    console .log( 'params', params );
+
+    return { 
+        props: {                //  Propiedad Obligatoria: Se pasara al componente dinamico, es decir como props a LinkDynamicComponent
+            link: params .link  //  Esta propiedad debe tener el mismo nombre del archivo que define el Componente Dinámico, en este caso [link].js 
+        } 
+    };
+    /**
+     * Debe usar getStaticProps si:
+     *  1. Los datos necesarios para representar la página están disponibles en el momento de la compilación antes de la solicitud del usuario. 
+     *  2. Los datos provienen de un CMS sin cabeza.
+     *  3. Los datos se pueden almacenar en caché públicamente (no específicos del usuario).
+     *  4. La página debe estar previamente renderizada (para SEO) y ser muy rápida: getStaticPropsgenera archivos HTML y JSON, los cuales pueden ser almacenados en caché por un CDN para mejorar el rendimiento.
+     */
+}
+
+/** Next.js pre-renderizará estáticamente todas las rutas especificadas */
+export async function getStaticPaths() {
+    
+    return { 
+        paths: [                        //  Propiedad Obligatoria: Hará disponible rutas estáticas a cada propiedad que se le pase
+            { params: { link: '1' } },  //  Esta propiedad debe tener el mismo nombre del archivo que define el Componente Dinámico, en este caso [link].js 
+            { params: { link: '2' } },  //  Esta propiedad debe tener el mismo nombre del archivo que define el Componente Dinámico, en este caso [link].js 
+            { params: { link: '3' } }   //  Esta propiedad debe tener el mismo nombre del archivo que define el Componente Dinámico, en este caso [link].js 
+        ],
+        fallback: false                 //  Propiedad Obligatoria: true muestra componente aun si no se encuentra la ruta, false obtendrá un 404
+    };
+    /** 
+     * Las rutas estáticas generadas para este caso serán:
+     *  - http://localhost:3000/links/1
+     *  - http://localhost:3000/links/2
+     *  - http://localhost:3000/links/3
+     */
+}
+  
+
 /** Dynamic Component */
-const LinkDynamicComponent = () => {
+const LinkDynamicComponent = ({ link }) => {
 
     return (
         <Layout>
-            <h1>Component with Dynamic Routing <strong>[link].js</strong></h1>
+            <h1>Component with Dynamic Routing <strong>[{ link }].js</strong></h1>
         </Layout>
     );
 }
