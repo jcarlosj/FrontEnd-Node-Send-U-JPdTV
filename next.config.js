08a725a39a7e3a4c306a05b776dba87886fe7eb0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-17 11:21:22 -0500

    [-] Project: Change value of environment variables for BackEnd and FrontEnd URLs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** next.config.js is the name of the file where environment variables should be defined in NextJS */
 module .exports = {
     env: {
-        backendURL: 'http://localhost:4000',
-        frontendURL: 'http://localhost:3000'
+        backendURL: 'https://boiling-coast-32679.herokuapp.com',
+        frontendURL: 'https://node-send-frontend.vercel.app'
     }
 }
